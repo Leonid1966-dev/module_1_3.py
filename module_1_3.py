7bb1f0b642998be3c0535f588c3734765e504bfe
Leonid1966-dev <rskle66@gmail.com>
2024-11-11 22:31:35 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+"module_1_3.py "
+name = 'Leonid'
+print('Name:', name)
+age = 58
+print('Age:', age)
+newage = (age + 10)
+print('New age:', newage)
+is_studen = True
+print('is Student:', is_studen)
